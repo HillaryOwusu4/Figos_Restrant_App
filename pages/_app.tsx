ce3c480fb117db36e82c05b31c4360a216dc314c
HillaryOwusu23 <124349494+HillaryOwusu23@users.noreply.github.com>
2023-10-14 15:13:07 -0600

    removed middleware

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Mainpage from '../components/Layout/Mainpage'
 import { Provider } from "react-redux";
 import { store,wrapper } from '../components/store/redux';
-import middleware from '../middleware';
+
 function App({ Component, pageProps }:any) {
   return( <Mainpage>
     <Provider store={store}> 
